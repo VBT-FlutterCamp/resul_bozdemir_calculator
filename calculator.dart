7b69231f4037662d93b12cce9c00fc40772b5859
resulbdev <resulbozdemirr@gmail.com>
2022-02-15 14:42:39 +0300

    Create calculator.dart

---
+++ calculator.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+void main() {
+  print("Hoşgeldiniz");
+  print("Toplama yapmak için '1' ve entera basın");
+  print("Çıkarma yapmak için '2' ve entera basın");
+  print("Çarpma yapmak için '3' ve entera basın");
+  print("Bölme yapmak için '4' ve entera basın");
+
+  int islem = int.parse(stdin.readLineSync()!);
+
+  switch (islem) {
+    case 1:
+      {
+        print("Lütfen 2 adet sayı giriniz");
+        Topla(
+            int.parse(stdin.readLineSync()!), int.parse(stdin.readLineSync()!));
+        break;
+      }
+    case 2:
+      {
+        Cikarma(
+            int.parse(stdin.readLineSync()!), int.parse(stdin.readLineSync()!));
+        break;
+      }
+    case 3:
+      {
+        Carpma(
+            int.parse(stdin.readLineSync()!), int.parse(stdin.readLineSync()!));
+        break;
+      }
+    case 3:
+      {
+        Bolme(
+            int.parse(stdin.readLineSync()!), int.parse(stdin.readLineSync()!));
+        break;
+      }
+  }
+}
+
+void Topla(int sayi1, int sayi2) {
+  print("Toplam = ${sayi1 + sayi2}");
+}
+
+void Cikarma(int sayi1, int sayi2) {
+  print("Toplam = ${sayi1 - sayi2}");
+}
+
+void Carpma(int sayi1, int sayi2) {
+  print("Toplam = ${sayi1 * sayi2}");
+}
+
+void Bolme(int sayi1, int sayi2) {
+  print("Toplam = ${sayi1 / sayi2}");
+}
